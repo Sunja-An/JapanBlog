cc4a727f86666d085532571c83bfec39f17451f2
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2024-09-23 22:27:55 +0900

    Feat: Fade-in-out animation added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,40 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      keyframes: {
+        "fade-in": {
+          "0%": {
+            opacity: "0",
+          },
+          "100%": {
+            opacity: "1",
+          },
+        },
+        "fade-in-up": {
+          "0%": {
+            opacity: "0",
+            transform: "translate3d(0, 100%, 0)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translate3d(0, 0, 0)",
+          },
+        },
+        "slide-in-up": {
+          "0%": {
+            visibility: "visible",
+            transform: "translate3d(0, 100%, 0)",
+          },
+          "100%": {
+            transform: "translate3d(0, 0, 0)",
+          },
+        },
+      },
+      animation: {
+        fadein: "fade-in 1s ease-in-out 0.25s 1",
+        fadeinup: "fade-in-up 1s ease-in-out 0.25s 1",
+        slideinup: "slide-in-up 1s ease-in-out 0.25s 1",
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
