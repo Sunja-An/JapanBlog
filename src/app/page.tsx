c26bede1369baf67154e5291c42b8d620cf64e8a
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2024-09-23 22:29:04 +0900

    Feat: Main Page Category & Photo Card added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
-export default function JapanBlog_Blog_Page() {
+import Client_PhotoCard from "@/app/Client_PhotoCard";
+import Client_Category from "@/app/Client_Category";
+
+export default async function JapanBlog_Blog_Page() {
   return (
-    <div className="">
-      <div></div>
-    </div>
+    <main className="px-3 pb-40 w-full h-fit flex flex-col justify-start items-start">
+      <section className="w-full h-screen flex justify-center items-center">
+        <div className="relative w-11/12 h-full">
+          <Client_PhotoCard sort="tech" linkId={0} pictureUrl="" />
+        </div>
+      </section>
+      <section className="w-full h-screen flex flex-col justify-center items-center shrink">
+        <Client_Category />
+      </section>
+    </main>
   );
 }
