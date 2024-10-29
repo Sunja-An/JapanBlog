82163382784d806f963b5705c56fa87bf70401c9
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2024-10-29 20:23:43 +0900

    Feat: Tech Id Page Edited Type Changed

---
+++ src/app/tech/[id]/Client_Content.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface ITechBlogContent {
+  content: string;
+}
+
+export default function Client_TechBlog_Content() {
+  return (
+    <div className="w-full h-fit flex flex-col shrink-0 flex-wrap justify-start items-start">
+      <p className="font-pretendard text-lg text-black">
+        L'esadsasdL'esadsasdL'esadsasdL'esadsasdL'esadsasdL'esadsasdL'esadsasdL'
+        esadsasdL'esadsasdL'esadsasdL'esadsasdL'esadsasdL'esadsasdL'esadsasdL'esadsasdL'
+        esadsasdL'esadsasdL'esadsasdL'esadsasdL'esadsasdL'esadsasdL'esadsasdL'esadsasdL'esadsasd
+      </p>
+    </div>
+  );
+}
--- src/app/tech/[id]/page.tsx
+++ src/app/tech/[id]/page.tsx
@@ -1,8 +1,11 @@
 import TechPostHeader from "@/layout/techBlog/TechPostHeader";
 
+import Client_TechBlog_Content from "@/app/tech/[id]/Client_Content";
+import Client_ShareBtns from "@/app/tech/[id]/Client_ShareBtns";
+
 export default async function JapanBlog_Tech_Single_Page() {
   return (
-    <main className="w-full h-full flex flex-col justify-start items-start gap-20">
+    <main className="pb-40 w-full h-full flex flex-col justify-start items-start gap-20">
       <TechPostHeader imageUrl="/public/image/LogoImage.png" />
       <section className="w-full h-fit flex flex-col jusitfy-start items-start">
         <div className="w-full h-40 flex justify-start items-center">
@@ -17,6 +20,12 @@ export default async function JapanBlog_Tech_Single_Page() {
             </span>
           </div>
         </div>
+      </section>
+      <section className="px-10 w-full h-fit flex flex-col justify-center items-center">
+        <Client_TechBlog_Content />
+      </section>
+      <section className="">
+        <Client_ShareBtns id={1} />
       </section>
     </main>
   );
