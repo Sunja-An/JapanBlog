d7d13b0e6de07103a564961e686ded28f6bab0aa
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-01-24 15:19:59 +0900

    Feat: Add Widget layer objects ( Category, Flag Button )

---
+++ src/widgets/category/Category.tsx
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+import { CategoryContent } from "../../shared/constants";
+
+function Category() {
+  return (
+    <Wrapper>
+      {CategoryContent.map((item, key: number) => {
+        return (
+          <CategoryBox key={key}>
+            <CategoryHeader>{item.title}</CategoryHeader>
+            {item.content.map((item: string, key: number) => {
+              return <CategorySubTitle key={key}>{item}</CategorySubTitle>;
+            })}
+          </CategoryBox>
+        );
+      })}
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  padding: 30px;
+  width: 300px;
+  height: 100%;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: flex-start;
+  align-items: flex-start;
+  border: 10px;
+  border-color: black;
+  border-radius: 18px;
+  box-shadow: 8px 8px 4px gray;
+  @media (max-width: 1024px) {
+    display: none;
+  }
+`;
+
+const CategoryBox = styled.div`
+  width: 100%;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 8px;
+`;
+
+const CategoryHeader = styled.h2`
+  font-size: 18px;
+  font-weight: 700;
+  color: black;
+`;
+
+const CategorySubTitle = styled.span`
+  font-size: 12px;
+  font-weight: 500;
+  color: black;
+  cursor: pointer;
+`;
+
+export { Category };
