cbd28a4c313e593289bf1e966a6b86d5a14a878b
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-01-24 15:19:28 +0900

    Feat: Add Header

--- src/shared/components/header/Header.tsx
+++ src/shared/components/header/Header.tsx
@@ -1,14 +1,34 @@
 import styled from "styled-components";
 
+import { SONULogo } from "../../constants";
+import { useNavigate } from "react-router-dom";
+
 function Header() {
+  const router = useNavigate();
+
+  const onClickRouting = (url: string) => {
+    router(`/${url}`);
+  };
+
   return (
     <Wrapper>
       <NavContainer>
         <NavBox>
-          <NavContent>introduce</NavContent>
+          <NavContent onClick={() => onClickRouting("introduce")}>
+            introduce
+          </NavContent>
         </NavBox>
+        <LogoBox>
+          <Logo
+            src={SONULogo}
+            alt="logo"
+            onClick={() => onClickRouting("blog")}
+          />
+        </LogoBox>
         <NavBox>
-          <NavContent>portfolio</NavContent>
+          <NavContent onClick={() => onClickRouting("portfolio")}>
+            portfolio
+          </NavContent>
         </NavBox>
       </NavContainer>
     </Wrapper>
@@ -16,17 +36,25 @@ function Header() {
 }
 
 const Wrapper = styled.header`
+  width: 100%;
   min-height: 120px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const NavContainer = styled.nav`
-  padding: 20px 120px;
-  width: 100%;
+  padding: 20px 20px;
+  width: 80%;
   height: 100%;
   display: flex;
   flex-flow: row wrap;
   justify-content: space-between;
   align-items: center;
+  @media (max-width: 768px) {
+    justify-content: center;
+    gap: 40px;
+  }
 `;
 
 const NavBox = styled.ul`
@@ -34,6 +62,9 @@ const NavBox = styled.ul`
   display: flex;
   justify-content: center;
   align-items: center;
+  @media (max-width: 768px) {
+    display: none;
+  }
 `;
 
 const NavContent = styled.li`
@@ -46,6 +77,24 @@ const NavContent = styled.li`
   font-weight: 600;
   color: black;
   text-transform: uppercase;
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const LogoBox = styled.div`
+  width: 120px;
+  height: 120px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Logo = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 100%;
 `;
 
 export { Header };
