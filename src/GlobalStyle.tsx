d19ec3c848982690e8906a8618a359973a30389e
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-01-24 15:36:47 +0900

    Feat: Modify gradient

--- src/GlobalStyle.tsx
+++ src/GlobalStyle.tsx
@@ -2,7 +2,7 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
     body {
-        background: linear-gradient(45deg, #FFFAFA 0%, #EDE8E2 30%);
+        background: linear-gradient(to top, #EDE8E2 70%, #FFFAFA 100%);
     }
 `;
 
