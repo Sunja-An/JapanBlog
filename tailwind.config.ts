68371ed57aa16e7cff788535965cb45961b9ce56
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-02-01 22:27:40 +0900

    Feat: Add Bun Package manager & framer motion & rive components & next.js library about mdx

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,15 +2,29 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/views/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/widgets/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/shared/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      xl: { max: "1200px" },
+      lg: { max: "1074px" },
+      md: { max: "768px" },
+      sm: { max: "640px" },
+    },
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      backgroundImage: {
+        linearground: "linear-gradient(to bottom, #FFFAFA 0%, #EDE8E2 30%)",
+      },
+      fontFamily: {
+        pretendardJP: "var(pretendard-JP)",
+        pretendard: "var(pretendard-Regular)",
       },
     },
   },
